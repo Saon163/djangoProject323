d353fb016ef0e98816afc4abdbfd6a627d1f208d
Saon163 <126964844+Saon163@users.noreply.github.com>
2023-03-23 12:50:28 +0900

    sujung

--- polls/urls.py
+++ polls/urls.py
@@ -35,3 +35,20 @@ urlpatterns = [
     path('3/', views.for_loop, name = 'for_loop'),
     path('4/', views.ifequal_func, name = 'ifequal_function'),
     ]
+
+from django.urls import path
+
+from polls import views
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('2/', views.if_else, name = 'if_else'),
+    path('3/', views.for_loop, name = 'for_loop'),
+    path('4/', views.ifequal_func, name = 'ifequal_function'),
+    path('5/', views.filter_func, name = 'filter_function'),
+    path('today/', views.today_is, name = 'today_is'),
+    path('home/', views.home, name = 'home'),
+    path('blog/',views.blog, name = 'blog'),
+    path('tablecss/', views.index2, name = 'index2'),
+    path('contact/',views.contact, name = 'contact'),
+]
--- polls/views.py
+++ polls/views.py
@@ -45,3 +45,64 @@ def ifequal_func(request):
         'varb' : varb,
         }
     return HttpResponse(temp.render(context1, request))
+
+def filter_func(request):
+    name = "HUFS Yongin"
+    weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    temp = loader.get_template('hello_filter.html')
+    context = {
+        'name' : name,
+        'weekday' : weekday,
+        }
+    return HttpResponse(temp.render(context, request))
+
+def today_is(request):
+    now = datetime.datetime.now()
+    temp = loader.get_template('blog/datetime.html')
+    context = {
+        'now' : now,
+    }
+    return HttpResponse(temp.render(context, request))
+
+from django.conf import settings
+
+def home(request):
+    now = datetime.datetime.now()
+    temp = loader.get_template('blog2/home.html')
+    context = {
+        'now' : now,
+        'base_dir': settings.BASE_DIR,
+    }
+    return HttpResponse(temp.render(context, request))
+
+def blog(request):
+    writer = "Bernardo"
+    temp = loader.get_template('blog2/blog.html')
+    context = {
+        'writer' : writer,
+    }
+    return HttpResponse(temp.render(context, request))
+
+def index2(request):
+    name = 'HUFS'
+    cities = [
+    {'name': 'Mumbai', 'population': '19,000,000', 'country': 'India'},
+    {'name': 'Calcutta', 'population': '15,000,000', 'country': 'India'},
+    {'name': 'New York', 'population': '20,000,000', 'country': 'USA'},
+    {'name': 'Chicago', 'population': '7,000,000', 'country': 'USA'},
+    {'name': 'Tokyo', 'population': '33,000,000', 'country': 'Japan'},
+    ]
+    temp = loader.get_template('tablecss.html')
+    context = {
+        'name' : name,
+        'cities' : cities,
+        }
+    return HttpResponse(temp.render(context, request))
+
+def contact(request):
+    writer = "Bernardo"
+    temp = loader.get_template('contact.html')
+    context = {
+        'writer' : writer,
+    }
+    return HttpResponse(temp.render(context, request))
